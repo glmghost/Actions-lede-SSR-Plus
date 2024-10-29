059662f3fd3d5c55a56517025b748014dc1ea59f
glmghost <62704244+glmghost@users.noreply.github.com>
2024-10-29 10:40:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,5 +16,6 @@
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-echo '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+# echo '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# echo '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
